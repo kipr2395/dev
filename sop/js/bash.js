08cdb5c06d4841e1290a8530c213a888456058ab
Kimberly Preston <kimberlypreston@Kimberlys-MacBook-Pro-2.local>
2017-02-01 20:44:15 -0700

    added content

---
+++ sop/js/bash.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+		$("ul").hide();
+
+		 $(".one").click(function(){
+        $(".firstlist").show();
+        $("div").remove("#bone");
+    });
+		 $(".two").click(function(){
+        $(".secondlist").show();
+        $("div").remove("#btwo");
+    });
+
+
+});
+
